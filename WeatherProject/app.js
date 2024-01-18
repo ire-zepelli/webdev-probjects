68f0bfba1ebc9cd80b4fde99332fc74416fb3ac6
Benedict Avenido <benedictavenido1320@gmail.com>
2024-01-18 18:53:25 +0800

    Simple Weather App

---
+++ WeatherProject/app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req,res)=>{
+
+    res.sendFile(__dirname + "/index.html");
+
+});
+
+app.post("/", (req,res)=>{
+
+    console.log("Post request recieved.");
+    const query = req.body.cityName;
+    const apiKey = "5bc4f598204358e7141e19457c5db276";
+    const unit = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&units="+ unit +"&appid=" + apiKey;
+
+    https.get(url, (response)=>{
+        console.log(response.statusCode); 
+        
+        response.on("data", (data)=>{
+            const weatherData = JSON.parse(data);
+            
+            const temp = weatherData.main.temp;
+            const icon = weatherData.weather[0].icon;
+            const imageURL = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+            const weatherDescription = weatherData.weather[0].description;
+            console.log(weatherData);
+
+            res.write(`<h1>The temperature in ${query} is ${temp} Celcius</h1>`)
+            res.write(`<p>The weather is currently ${weatherDescription}</p>`);
+            res.write(`<img src="${imageURL}" alt="weatherImage">`);
+            res.send();
+        });
+    });
+});
+
+app.listen(3000, ()=>{
+    console.log("Server is running on port 30000");
+});
